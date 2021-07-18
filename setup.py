d8c36b37be47f5752926f8705377fd5412dcb039
ray <ray2012789852@gmail.com>
2021-07-18 16:25:01 +0800

    ci: update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
    description='A Python library for collecting articles from ptt website',
    author='rayhzh',
    author_email='ray2012789852@gmail.com',
+   url = 'https://github.com/rayhzh/pttpost-parser',
    packages=['ptt'],
    install_requires=['requests', 'beautifulsoup4', 'tabulate'],
 )
