2dec3320b71efd06f192141fa8a74a9b818ff742
ray <ray2012789852@gmail.com>
2021-07-18 16:29:06 +0800

    ci: add tqdm package

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
    name='pttpost-parser',
-   version='0.0.1',
+   version='0.0.2',
    description='A Python library for collecting articles from ptt website',
    author='rayhzh',
    author_email='ray2012789852@gmail.com',
    url = 'https://github.com/rayhzh/pttpost-parser',
    packages=['ptt'],
-   install_requires=['requests', 'beautifulsoup4', 'tabulate'],
+   install_requires=['requests', 'beautifulsoup4', 'tabulate', 'tqdm'],
 )
